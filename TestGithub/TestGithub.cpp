e9b82260f4da5a86aad3389606e401f275d170d5
Test <kirolos_165762@fci.sohag.edu.eg>
2021-10-18 18:26:26 +0200

    My Project

---
+++ TestGithub/TestGithub.cpp
@@ -0,0 +1,11 @@
+// TestGithub.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    std::cout << "welcome to github";
+}
+
